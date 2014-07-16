139e61eb5747e11181f0b3bf4daaec42f63ac0cf
Wouter van Atteveldt <wouter@vanatteveldt.com>
2014-07-16 20:32:14 +0200

    updating to corenlp 3.4 (in progress)

--- tests/test_corenlp.py
+++ tests/test_corenlp.py
@@ -1,75 +1,91 @@
 """
-Test the Stanford corenlp parser and Semafor semantic parser
+Test the CoreNLP parser/lemmatizer functions and task.
 """
 
-import json
-import os
-import csv
-import socket
+import logging
 from unittest import SkipTest
+import os.path
 
-from nose.tools import assert_equal, assert_not_equal
+from nose.tools import assert_equal, assert_not_equal, assert_in
 
-def _check_corenlp_home():
-    if not os.environ.get("CORENLP_HOME"):
-        raise SkipTest("Cannot find CORENLP_HOME")
-
-
-def _check_semafor():
-    from xtas.tasks.semafor import get_settings
-    host, port = get_settings()
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
-        result = s.connect_ex((host, port))
-        if(result != 0) :
-            raise SkipTest("Semafor web service not listening to {host}:{port}"
-                           .format(**locals()))
-    finally:
-        s.close()
-
-def test_to_conll():
-    from xtas.tasks.corenlp import to_conll
-    _check_corenlp_home()
-
-    tree = ("(ROOT (S (NP (DT The) (JJ second) (NN miner)) "
-            "(VP (VBD was) (VP (VBN found) "
-            "(PP (IN in) (NP (DT the) (NN afternoon))))) (. .)))")
-
-    result = to_conll(tree)
-
-    result = [row for row in csv.reader(result.split("\n"), delimiter="\t")
-              if row]
-    assert_equal(len(result), 9)
-    assert_equal(result[0][:4], ['1', 'The', '_', 'DT'])
+from xtas.tasks.corenlp import parse, stanford_to_saf, get_corenlp_version
+from xtas.tasks.single import corenlp, corenlp_lemmatize
 
 
-def test_corenlp_semafor():
-    "Try corenlp + semafor pipe. Very slow test due to corenlp loading time."
-    from xtas.tasks.corenlp import parse_text
-    from xtas.tasks.semafor import get_frames
-    _check_semafor()
-    _check_corenlp_home()
+def _check_corenlp():
+    v = get_corenlp_version()
+    if not v:
+        raise SkipTest("CoreNLP not found at CORENLP_HOME")
 
-    sent = "John loves Mary"
-    try:
-        saf = json.load(open("/tmp/test_corenlp.json"))
-    except:
-        saf = parse_text(sent)
-        json.dump(saf, open("/tmp/test_corenlp.json", "w"))
 
-    assert_equal(set(t['lemma'] for t in saf['tokens']),
-                 {'John', 'love', 'Mary'})
-    tokens = {token['id'] : token['lemma'] for token in saf['tokens']}
-    deps = set((tokens[t['child']], t['relation'], tokens[t['parent']])
-               for t in saf['dependencies'])
-    assert_equal(set(tokens.values()), {'John', 'love', 'Mary'})
-    assert_equal(deps, {('John', 'nsubj', 'love'),
-                        ('Mary', 'dobj', 'love')})
-    frames = list(get_frames(saf))
+def test_parse_xml():
+    xml = open(os.path.join(os.path.dirname(__file__), "test_corenlp.xml")).read()
+    saf = stanford_to_saf(xml)
+    assert_equal({t['lemma'] for t in saf['tokens']},
+                 {"John", "attack", "I", "in", "London", "hit", "he", "back", "."})
+    london = [t for t in saf['tokens'] if t['lemma'] == 'London'][0]
+    assert_equal(london['pos'], 'NNP')
+    assert_in({"type": "LOCATION", "tokens": [london['id']]}, saf['entities'])
 
-    assert_equal(len(frames), 1)
-    assert_equal(frames[0]['name'], 'Experiencer_focus')
-    assert_equal({tokens[t] for t in frames[0]['target']}, {'love'})
 
-if __name__ == '__main__':
-    test_corenlp_semafor()
+def test_lemmatize():
+    _check_corenlp()
+    lines = parse("He jumped. \n\n Cool!",
+                  annotators=['tokenize', 'ssplit', 'pos', 'lemma'])
+    saf = stanford_to_saf(lines)
+    assert_equal(set(saf.keys()), {'tokens', 'header'})
+
+    assert_equal({t['lemma'] for t in saf['tokens']},
+                 {'he', 'jump', 'cool', '!', '.'})
+    assert_equal({t['sentence'] for t in saf['tokens']},
+                 {1, 2})
+
+
+def test_lemmatize_unicode():
+    _check_corenlp()
+    lines = parse(u"\u0540\u0578\u057e\u056b\u056f hit C\xe9sar",
+                  annotators=['tokenize', 'ssplit', 'pos', 'lemma'])
+    saf = stanford_to_saf(lines)
+    assert_equal({t['lemma'] for t in saf['tokens']},
+                 {'Cesar', 'hit', 'Hovik'})
+
+
+def test_ner():
+    _check_corenlp()
+    annotators = ['tokenize', 'ssplit', 'pos', 'lemma', 'ner']
+    saf = stanford_to_saf(parse("John lives in Amsterdam",
+                                annotators=annotators))
+    lemmata = {t['id']: t['lemma'] for t in saf['tokens']}
+    entities = {lemmata[e['tokens'][0]]: e['type'] for e in saf['entities']}
+    assert_equal(entities, {'John': 'PERSON', 'Amsterdam': 'LOCATION'})
+
+
+def test_parse():
+    _check_corenlp()
+    saf = stanford_to_saf(parse("John loves himself"))
+    lemmata = {t['id']: t['lemma'] for t in saf['tokens']}
+    assert_equal(saf['trees'], [{
+        "tree": "(ROOT (S (NP (NNP John)) (VP (VBZ loves) "
+                "(NP (PRP himself)))))",
+        "sentence": 1
+        }])
+    deps = {(lemmata[d['child']], lemmata[d['parent']], d['relation'])
+            for d in saf['dependencies']}
+    assert_equal(deps, {('John', 'love', 'nsubj'),
+                        ('himself', 'love', 'dobj')})
+    corefs = {tuple(sorted([lemmata[c[0][0]], lemmata[c[1][0]]]))
+              for c in saf['coreferences']}
+    assert_equal(corefs, {tuple(sorted(['John', 'himself']))})
+
+
+def test_task():
+    _check_corenlp()
+    raw = corenlp_lemmatize("It works", output='raw')
+    assert_in("<lemma>work</lemma>", raw)  # list of header / sentence / tokens
+    saf = corenlp_lemmatize("It works", output='saf')
+    assert_equal(len(saf['tokens']), 2)
+
+    raw = corenlp("It works", output='raw')
+    assert_in('<dep type="nsubj">', raw)
+    saf = corenlp("It works", output='saf')
+    assert_equal(len(saf['dependencies']), 1)
--- xtas/tasks/corenlp.py
+++ xtas/tasks/corenlp.py
@@ -1,296 +1,189 @@
 """
-Python interface for the Stanford CoreNLP suite and conversion to xtas/SAF
+Wrapper around the CoreNLP parser
 
-CoreNLP requires CoreNLP to be installed, and an environment variable
-CORENLP_HOME pointing to the folder where it is installed.
+The module expects CORENLP_HOME to point to the CoreNLP installation dir.
 
-See http://nlp.stanford.edu/software/corenlp.shtml
-Download e.g.
+If run with all annotators, it requires around 3G of memory,
+and it will keep the process in memory indefinitely.
+
+See: http://nlp.stanford.edu/software/corenlp.shtml#Download
+
+Tested with
 http://nlp.stanford.edu/software/stanford-corenlp-full-2014-01-04.zip
 """
 
-import os
-import re
-import logging
-import pexpect
-import itertools
+
 import datetime
-import tempfile
+import io
+import itertools
+import logging
+import os
+import os.path
+import re
 import subprocess
-import json
+import threading
+import time
+import collections
+
+from corenlp_xml.document import Document
+
+from six import iteritems
 
 from unidecode import unidecode
 
-from cStringIO import StringIO
-
-from .saf import SAF
-
 log = logging.getLogger(__name__)
-import threading
 
-PARSERS = {}
-PARSER_LOCK = threading.Lock()
-CLASSNAME = "edu.stanford.nlp.pipeline.StanfordCoreNLP"
-
-
-def parse_text(text, **options):
-    """Use a global/persistent corenlp object to parse the given text"""
-    global PARSER
-    key = json.dumps(options)
-    with PARSER_LOCK:
-        if key not in PARSERS:
-            PARSERS[key] = StanfordCoreNLP(**options)
-        parse = PARSERS[key].parse(text)
-    return StanfordCoreNLP.stanford_to_saf(parse)
+_CORENLP_VERSION = None
 
 
 class StanfordCoreNLP(object):
 
-    corenlp_version = os.environ.get("CORENLP_VERSION", "3.3.1")
+    _singletons = {}  # annotators : object
 
-    def __init__(self, timeout=600, annotators=None, **classpath_args):
+    @classmethod
+    def get_singleton(cls, annotators=None, **options):
         """
-        Start the CoreNLP server as a java process. Arguments are used to
-        locate the correct jar files.
-
-        @param corenlp_path: the directory containing the jar files
-        @param corenlp_version: the yyyy-mm-dd version listed in the jar name
-        @param models_version: if different from the main version, the version
-                               date of the models jar
-        @param timeout: Time to wait for a parse before giving up
+        Get or create a corenlp parser with the given annotator and options
+        Note: multiple parsers with the same annotator and different options
+              are not supported.
         """
+        if annotators is not None:
+            annotators = tuple(annotators)
+        if annotators not in cls._singletons:
+            cls._singletons[annotators] = cls(annotators, **options)
+        return cls._singletons[annotators]
 
-        self.timeout = timeout
-        cmd = self.get_command(classname=CLASSNAME, memory="3G",
-                               annotators=annotators, **classpath_args)
-
-        log.info("Starting the Stanford Core NLP parser.")
-        log.debug("Command: {cmd}".format(**locals()))
-        self._corenlp_process = pexpect.spawn(cmd)
-        self._wait_for_corenlp_init()
-
-    def _wait_for_corenlp_init(self):
-        self._corenlp_process.expect("Entering interactive shell.", timeout=600)
-        log.info("NLP tools loaded.")
-
-    def _get_results(self):
+    def __init__(self, annotators=None, timeout=1000, memory="3G"):
         """
-        Get the raw results from the corenlp process
+        Start the CoreNLP server with a system call.
+
+        @param annotators: Which annotators to use, e.g.
+                           ["tokenize", "ssplit", "pos", "lemma"]
+        @param memory: Java heap memory to use
         """
-        buff = StringIO()
+        global _CORENLP_VERSION
+        self.annotators = annotators
+        self.memory = memory
+        _CORENLP_VERSION = get_corenlp_version()
+        self.start_corenlp()
+
+    def start_corenlp(self):
+        cmd = get_command(memory=self.memory, annotators=self.annotators)
+        log.warn("Starting corenlp: {cmd}".format(**locals()))
+        self.corenlp_process = subprocess.Popen(cmd, shell=True,
+                                                stdin=subprocess.PIPE,
+                                                stdout=subprocess.PIPE,
+                                                stderr=subprocess.PIPE)
+        self.lock = threading.Lock()
+        self.read_thread = threading.Thread(target=self.read_output_lines)
+        self.read_thread.daemon = True
+        self.read_thread.start()
+        log.warn("Waiting for prompt")
+        self.communicate(input=None, wait_for_output=False)
+
+    def read_output_lines(self):
+        "intended to be run as background thread to collect parser output"
         while True:
-            try:
-                incoming = self._corenlp_process.read_nonblocking(2000, 1)
-            except pexpect.TIMEOUT:
-                log.debug("Waiting for CoreNLP process; buffer: {!r} "
-                          .format(buff.getvalue()))
-                continue
+            chars = self.corenlp_process.stdout.readline()
+            if chars == '':  # EOF
+                break
+            self.out.write(chars)
 
-            for ch in incoming:
-                if ch == "\n":  # return a found line
-                    yield buff.getvalue()
-                    buff.seek(0)
-                    buff.truncate()
-                elif ch not in "\r\x07":
-                    buff.write(ch)
-                    if ch == ">" and buff.getvalue().startswith("NLP>"):
-                        return
+    def communicate(self, input, wait_for_output=True):
+        log.warn("Sending {} bytes to corenlp".format(input and len(input)))
+        if self.corenlp_process.poll() is not None:
+            logging.warn("CoreNLP process died, respawning")
+            self.start_corenlp()
+        with self.lock:
+            self.out = io.BytesIO()
+            if input:
+                self.corenlp_process.stdin.write(input)
+                self.corenlp_process.stdin.write("\n\n")
+                self.corenlp_process.stdin.flush()
+
+            # wait until we get a prompt
+            logging.warn("Waiting for NLP>")
+            err_buffer = io.BytesIO()
+            while True:
+                char = self.corenlp_process.stderr.read(1)
+                err_buffer.write(char)
+                err_buffer.seek(-5, 2)
+                if err_buffer.read() == "NLP> ":
+                    break
+
+        # give stdout a chance to flush
+        # is there a better way to do this?
+        while True:
+            time.sleep(.1)
+            result = self.out.getvalue()
+            if result or not wait_for_output:
+                return result
 
     def parse(self, text):
         """Call the server and return the raw results."""
-        # clean up anything leftover
-        while True:
-            try:
-                self._corenlp_process.read_nonblocking(4000, 0.3)
-            except pexpect.TIMEOUT:
-                break
-
-        if not isinstance(text, unicode):
+        if isinstance(text, bytes):
             text = text.decode("ascii")
         text = re.sub("\s+", " ", unidecode(text))
-        self._corenlp_process.sendline(text)
-
-        return self._get_results()
-
-    @classmethod
-    def get_command(cls, classname, argstr="", memory=None, annotators=None,
-                    **classpath_args):
-        classpath = cls.get_classpath(**classpath_args)
-        memory = "" if memory is None else "-Xmx{memory}".format(**locals())
-        if isinstance(argstr, list):
-            argstr = " ".join(map(str, argstr))
-        if annotators:
-            if isinstance(annotators, list):
-                annotators = ",".join(annotators)
-            argstr += " -annotators {annotators} ".format(**locals())
-        return ("java {memory} -cp {classpath} {classname} {argstr}"
-                .format(**locals()))
-
-    @classmethod
-    def get_classpath(cls, corenlp_path=None, corenlp_version=None,
-                      models_version=None):
-        if corenlp_path is None:
-            corenlp_path = os.environ["CORENLP_HOME"]
-        if corenlp_version is not None:
-            cls.corenlp_version = corenlp_version
-        if models_version is None:
-            models_version = cls.corenlp_version
-
-        jars = ["stanford-corenlp-{cls.corenlp_version}.jar"
-                .format(**locals()),
-                "stanford-corenlp-{models_version}-models.jar"
-                .format(**locals()),
-                "joda-time.jar", "xom.jar", "jollyday.jar"]
-        jars = [os.path.join(corenlp_path, jar) for jar in jars]
-
-        # check whether jars exist
-        for jar in jars:
-            if not os.path.exists(jar):
-                raise Exception("Error! Cannot locate {jar}"
-                                .format(**locals()))
-
-        return ":".join(jars)
-
-    @classmethod
-    def stanford_to_saf(cls, lines):
-        """
-        Convert stanfords 'interactive' text format to saf
-        Unfortunately, stanford cannot return xml in interactive mode, so we
-        need to parse their plain text format
-        """
-        article = SAF()  # collections.defaultdict(list)
-        processed = {'module': "corenlp",
-                     'module-version':
-                     cls.corenlp_version,
-                     "started": datetime.datetime.now().isoformat()}
-        article.header = {'format': "SAF",
-                          'format-version': "0.0",
-                          'processed': [processed]}
-
-        lines = iter(lines)
-        lines.next()  # skip first line (echo of sentence)
-        _parse_article(article, lines)
-        return article
+        return self.communicate(text + "\n")
 
 
-
-CONLL_CLASS = "edu.stanford.nlp.trees.EnglishGrammaticalStructure"
-
-def _get_tree(saf_article, sentence_id):
-    for tree in saf_article['trees']:
-        if int(tree['sentence']) == int(sentence_id):
-            return tree['tree']
-    raise ValueError("Sentence {sentence_id} not found in trees {trees}"
-                     .format(trees=saf_article['trees'], **locals()))
-
-def to_conll(trees):
-    """
-    Convert a parse tree from Penn (?) to conll
-    """
-
-    trees = ["<sentence>{t}</sentence>".format(t=t.encode("utf-8"))
-             for t in trees]
-    xml = ("<root><document><sentences>\n{trees}"
-           "\n</sentences></document></root>"
-           .format(trees="\n".join(trees)))
-    with tempfile.NamedTemporaryFile() as f:
-        f.write(xml)
-        f.flush()
-        cmd = StanfordCoreNLP.get_command(CONLL_CLASS,
-                                          ["-conllx", "-treeFile", f.name],
-                                          memory="512M")
-        open("/tmp/tree.xml", "w").write(xml)
-        p = subprocess.check_output(cmd, shell=True)
-
-    return [c for c in p.split("\n\n") if c.strip()]
-
-def _regroups(pattern, text, **kargs):
-    m = re.match(pattern, text, **kargs)
-    if not m:
-        raise Exception("Pattern {pattern!r} did not match text {text!r}"
-                        .format(**locals()))
-    return m.groups()
+def parse(text, annotators=None, **options):
+    s = StanfordCoreNLP.get_singleton(annotators, **options)
+    return s.parse(text, **options)
 
 
-def _parse_article(article, lines):
-    strip = lambda l: l.strip()
-    tokens = {}  # sentence_no, index -> token
-    while True:  # parse one sentence
-        try:
-            line = lines.next()
-        except StopIteration:
-            break
-        while not line.strip():
-            line = lines.next()  # skip leading blanks
-        if line == "Coreference set:":
-            break
-        sentence_no = int(_regroups("Sentence #\s*(\d+)\s+", line)[0])
-        text = lines.next()
+def get_corenlp_version():
+    "Return the corenlp version pointed at by CORENLP_HOME, or None"
+    corenlp_home = os.environ.get("CORENLP_HOME")
+    if corenlp_home:
+        for fn in os.listdir(corenlp_home):
+            m = re.match("stanford-corenlp-([\d.]+)-models.jar", fn)
+            if m:
+                return m.group(1)
 
-        log.debug("Parsing sentence {sentence_no}: {text!r}"
-                  .format(**locals()))
 
-        # Parse tokens
-        for i, s in enumerate(re.findall('\[([^\]]+)\]', lines.next())):
-            wd = dict(re.findall(r"([^=\s]*)=([^=\s]*)", s))
-            tokenid = len(tokens) + 1
-            if not "CharacterOffsetBegin" in wd:
-                continue
-            token = dict(id=tokenid, word=wd['Text'], lemma=wd.get('Lemma', '?'),
-                         pos=wd.get('PartOfSpeech', '?'), sentence=sentence_no,
-                         offset=wd["CharacterOffsetBegin"])
-            token['pos1'] = POSMAP[token['pos']]
-            tokens[sentence_no, i] = token
-            article.tokens.append(token)
-            if wd.get('NamedEntityTag', 'O') != 'O':
-                article.entities.append(dict(tokens=[tokenid],
-                                             type=wd['NamedEntityTag']))
-        # try to peek ahead to see if we have more than tokens
-        lines, copy = itertools.tee(lines)
-        try:
-            peek = copy.next()
-            if peek.startswith("Sentence #"):
-                continue
-        except StopIteration:
-            break
+def get_command(annotators=None, memory=None):
+    "Return the system (shell) call to run corenlp"
+    corenlp_home = os.environ.get("CORENLP_HOME")
+    if not corenlp_home:
+        raise Exception("CORENLP_HOME not set")
+    cmd = 'java'
+    if memory:
+        cmd += ' -Xmx{memory}'.format(**locals())
+    cmd += ' -cp "{}"'.format(os.path.join(corenlp_home, "*"))
+    cmd += " edu.stanford.nlp.pipeline.StanfordCoreNLP -outputFormat xml"
+    if annotators:
+        cmd += ' -annotators {}'.format(",".join(annotators))
+    return cmd
 
-        # Extract original tree
-        tree = " ".join(itertools.takewhile(lambda x: x,
-                                            itertools.imap(strip, lines)))
-        article.trees.append(dict(sentence=sentence_no, tree=tree))
 
-        def parse_dependency(line):
-            rfunc, parent, child = _regroups(RE_DEPENDENCY, line)
-            if rfunc != 'root':
-                parent, child = [tokens[sentence_no, int(j)-1]['id']
-                                 for j in (parent, child)]
-                article.dependencies.append(dict(child=child, parent=parent,
-                                                 relation=rfunc))
-        map(parse_dependency, itertools.takewhile(strip, lines))
+def stanford_to_saf(xml_bytes):
+    doc = Document(xml_bytes)
+    saf = collections.defaultdict(list)
+    
+    saf['header'] = {'format': "SAF",
+                     'format-version': "0.0",
+                     'processed':  {'module': "corenlp",
+                                    'module-version': _CORENLP_VERSION,
+                                    "started": datetime.datetime.now().isoformat()}
+                 }
+    for sent in doc.sentences:
+        saf['tokens'] += [dict(id=t.id, sentence=sent.id, offset=t.character_offset_begin,
+                               lemma=t.lemma, word=t.word,
+                               pos=t.pos, pos1=POSMAP[t.pos]) for t in sent.tokens]
+        saf['entities'] += [dict(tokens=[t.id], type=t.ner) for t in sent.tokens if t.ner not in (None, 'O')]
+        if sent.collapsed_ccprocessed_dependencies:
+            saf['dependencies'] += [dict(child=dep.dependent.idx, parent=dep.governor.idx, relation=dep.type)
+                                    for dep in sent.collapsed_ccprocessed_dependencies.links
+                                    if dep.type != 'root']
+    if doc.coreferences:
+        saf['coreferences'] = [[[t.id for t in m.tokens] for m in coref.mentions]
+                               for coref in doc.coreferences]
+    saf['trees'] = [dict(sentence=s.id, tree=s.parse_string.strip()) for s in doc.sentences if s.parse_string is not None]
 
-    # get coreferences
-    def get_coreference(lines):
-        for line in itertools.takewhile(lambda l: l != "Coreference set:",
-                                        lines):
-            if line.strip():
-                groups = _regroups(RE_COREF, line)
-                yield [map(int, re.sub("[^\\d,]", "", s).split(","))
-                       for s in groups]
-    while True:
-        corefs = list(get_coreference(lines))
-        if not corefs:
-            break
-        for coref in corefs:
-            sets = []
-            for sent_index, head_index, from_index, to_index in coref:
-                # take all nodes from .. to, place head first (False<True)
-                indices = sorted(range(from_index, to_index),
-                                 key=lambda i: (i != head_index, i))
-                sets.append([tokens[sent_index, i-1]['id'] for i in indices])
-            article.coreferences.append(sets)
+    # remove default and empty elements
+    return {k: v for (k, v) in iteritems(saf) if v != []}
 
-RE_DEPENDENCY = r"([\w+]+)\(.+-([0-9']+), .+-([0-9']+)\)"
-RE_COREF = r'\s*\((\S+)\) -> \((\S+)\), that is: \".*\" -> \".*\"'
 POSMAP = {'CC': 'C',
           'CD': 'Q',
           'DT': 'D',
@@ -327,13 +220,13 @@ POSMAP = {'CC': 'C',
           'WP': 'O',
           'WP$': 'O',
           'WRB': 'B',
-          ',' : '.',
-          '.' : '.',
-          ':' : '.',
-          '``' : '.',
-          '$' : '.',
-          "''" : '.',
-          "#" : '.',
-          '-LRB-' : '.',
-          '-RRB-' : '.',
+          ',': '.',
+          '.': '.',
+          ':': '.',
+          '``': '.',
+          '$': '.',
+          "''": '.',
+          "#": '.',
+          '-LRB-': '.',
+          '-RRB-': '.',
           }
