fe894f5afd9b8390a45a39a67ed3a452fb34f609
Wouter van Atteveldt <wouter@vanatteveldt.com>
2014-07-27 01:20:32 +0200

    make celery queue configurable

--- xtas/celeryconfig.py
+++ xtas/celeryconfig.py
@@ -2,10 +2,25 @@
 # xtas_celeryconfig module in the PYTHONPATH.
 
 import os
-BROKER_HOST=os.environ.get('XTAS_BROKER_HOST', '127.0.0.1')
+from kombu import Exchange, Queue
 
-BROKER_URL = 'amqp://{host}:{port}//'.format(host=BROKER_HOST, port=5672)
+
+BROKER_HOST=os.environ.get('XTAS_BROKER_HOST', '127.0.0.1')
+BROKER_USERNAME=os.environ.get('XTAS_BROKER_USERNAME', 'guest')
+BROKER_PASSWORD=os.environ.get('XTAS_BROKER_PASSWORD', 'guest')
+
+BROKER_URL = 'amqp://{user}:{passwd}@{host}:{port}//'.format(
+    user=BROKER_USERNAME, passwd=BROKER_PASSWORD, host=BROKER_HOST, port=5672)
 CELERY_RESULT_BACKEND = 'amqp'
+
+CELERY_QNAME = os.environ.get('XTAS_CELERY_QUEUE', 'amcat')
+CELERY_QUEUES = (
+    Queue(CELERY_QNAME, Exchange('default'), routing_key=CELERY_QNAME),
+)
+CELERY_DEFAULT_QUEUE = CELERY_QNAME
+CELERY_DEFAULT_EXCHANGE_TYPE = 'direct'
+CELERY_DEFAULT_ROUTING_KEY = CELERY_QNAME
+
 
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
